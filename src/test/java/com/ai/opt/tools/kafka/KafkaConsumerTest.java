7930ac81dfd7e2512eecdbf3208fc37dccd0ddae
JackieLiu <liutong6053@163.com>
2016-04-24 11:05:10 +0800

    部分改善kafka获取

--- src/test/java/com/ai/opt/tools/kafka/KafkaConsumerTest.java
+++ src/test/java/com/ai/opt/tools/kafka/KafkaConsumerTest.java
@@ -1,33 +1,48 @@
 package com.ai.opt.tools.kafka;
 
+import com.ai.opt.tools.kafka.exception.ConfigException;
+import com.ai.opt.tools.kafka.util.KafkaConf;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by jackieliu on 16/3/28.
  */
 public class KafkaConsumerTest {
 
-    public static void main(String[] args){
+    @Test
+    public void consumerTest() throws Exception {
+        KafkaConf.loadConf();
         Properties props = new Properties();
-        props.put("bootstrap.servers", "10.1.130.84:39091,10.1.130.85:39091,10.1.236.122:39091");
-        props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("session.timeout.ms", "30000");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConf.getProps().getProperty("bootstrap.servers"));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS, 30000l);
+        props.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY,"");
+//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000l);
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-//        consumer.subscribe(Arrays.asList("foo", "bar"));
-//        while (true) {
-//            ConsumerRecords<String, String> records = consumer.poll(100);
-//            for (ConsumerRecord<String, String> record : records)
-//                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
-//        }
-
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe("omckafka");
+        boolean isRunning = true;
+        while (isRunning) {
+            Map<String, ConsumerRecords<String, String>> records = consumer.poll(100);
+            System.out.println(records.size());
+            Iterator<String> keySet = records.keySet().iterator();
+            while (keySet.hasNext()){
+                ConsumerRecords<String, String> consumerRecords = records.get(keySet.next());
+                System.out.printf("recordKey = %s,topicName = %s \r\n",keySet.next(),consumerRecords.topic());
+                for (ConsumerRecord<String, String> record:consumerRecords.records()) {
+                    System.out.printf("==== offset = %d, key = %s, value = %s \r\n", record.offset(), record.key(), record.value());
+                }
+            }
+        }
+//        consumer.commit(true);
+        consumer.close();
     }
 }
